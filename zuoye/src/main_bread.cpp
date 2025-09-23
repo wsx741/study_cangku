d7e5ce7590f926b3b466936b83dd113676482aa9
zhaojing <2864567508@qq.com>
2025-09-23 17:24:40 +0800

    作业三

--- zuoye/src/main_bread.cpp
+++ zuoye/src/main_bread.cpp
@@ -1,11 +1,9 @@
 #include "../include/bread.h"
 
 int main() {
-    cout << "=== 制作白面包 ===" << endl;
     WhiteBread whiteBread;
     whiteBread.bake();
-    
-    cout << "\n=== 制作全麦面包 ===" << endl;
+
     WholeWheatBread wholeWheatBread;
     wholeWheatBread.bake();
     
